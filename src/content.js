823b2976674ad913b6d05fb9064ddba5f646eceb
willy thiago <128657856+willdev24@users.noreply.github.com>
2025-03-13 15:46:14 -0400

    okk

--- src/content.js
+++ src/content.js
@@ -1,9 +1,10 @@
 import React from "react";
+import  "./Content.css"
 
 const Content = ({titulo, children})=>{
 
     return(
-        <article>
+        <article className="CORPO">
            <h2>{titulo}</h2>
            {children}
         </article>
